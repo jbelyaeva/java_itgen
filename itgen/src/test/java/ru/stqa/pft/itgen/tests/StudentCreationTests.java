cc5b15f9c393bae7d0552624549f84cf918ac161
Sergey Poletaev <info.elekit@yandex.ru>
2020-01-23 18:36:34 +0300

    Update StudentCreationTests.java

--- itgen/src/test/java/ru/stqa/pft/itgen/tests/StudentCreationTests.java
+++ itgen/src/test/java/ru/stqa/pft/itgen/tests/StudentCreationTests.java
@@ -12,6 +12,5 @@ public class StudentCreationTests extends TestBase {
     app.getStudentHelper().addStudent();
     app.getStudentHelper().fillStudentForm(new StudentData("Миша", "0Мишин", "Ж", "11112022", "Бангладеш", "Пересвет", "(GMT+05:00) Азия/Ташкент", "111111111111", "skype", "https://web.chat2desk.com/chat/my?dialogID=9346556", "111111111111", "111111111111", "telega", "fb", "vk", "ok", "inst"));
     app.getStudentHelper().submitFamalyCreation();
-    
   }
 }
