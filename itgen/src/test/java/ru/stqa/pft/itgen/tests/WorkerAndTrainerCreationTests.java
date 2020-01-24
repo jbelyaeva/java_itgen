999ab01667641e2fa94a3cbe3f49689618ebe105
Sergey Poletaev <info.elekit@yandex.ru>
2020-01-25 02:20:51 +0300

    Create WorkerAndTrainerCreationTests.java

---
+++ itgen/src/test/java/ru/stqa/pft/itgen/tests/WorkerAndTrainerCreationTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.itgen.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.itgen.model.WorkerData;
+
+public class WorkerAndTrainerCreationTests extends TestBase {
+
+  @Test
+  public void testWorkerCreation() {
+    app.getNavigationHelper().gotoWorker();
+    app.getWorkerHelper().addWorker();
+    app.getWorkerHelper().fillWorkerForm(new WorkerData("Павел", "Абакумов", "eee+" + Math.round(Math.random() * 10) + "@gmail.com", "89035540414", "Администратор"));
+    app.getWorkerHelper().submitWorkerCreation();
+  }
+}
