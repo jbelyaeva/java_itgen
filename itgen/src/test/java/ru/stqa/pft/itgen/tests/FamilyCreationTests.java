3e9c794454f417c1223a9768c7e6460f1950e27d
Sergey Poletaev <info.elekit@yandex.ru>
2020-01-24 22:18:12 +0300

    Create FamilyCreationTests.java

---
+++ itgen/src/test/java/ru/stqa/pft/itgen/tests/FamilyCreationTests.java
@@ -0,0 +1,11 @@
+package ru.stqa.pft.itgen.tests;
+
+import org.testng.annotations.Test;
+
+public class FamilyCreationTests extends TestBase {
+
+  @Test
+  public void testFamilyCreation() {
+
+  }
+}
