0d951e413caaed21db25ad5ab06f6b610b2d5811
Sergey Poletaev <info.elekit@yandex.ru>
2020-03-16 14:16:38 +0300

    Update ParentModificationTests.java

--- itgen/src/test/java/ru/stqa/pft/itgen/tests/ParentModificationTests.java
+++ itgen/src/test/java/ru/stqa/pft/itgen/tests/ParentModificationTests.java
@@ -12,7 +12,7 @@ public class ParentModificationTests extends TestBase {
     app.getStudentHelper().selectedFamily();
     app.getStudentHelper().selectedParent();
     app.getStudentHelper().modifyParent();
-    app.getStudentHelper().ModifyParentForm(new ParentData("Вова", "Ласточкин", "Венгрия", "Высоково", "(GMT-06:00) Америка/Бойла, Северная Дакота", "Английский", "84958887856", "домик", null, "ггггггг", "89995632129", "74965477823", "telega", "бук", "контакт", "одноклассники", "стаг", "444ггггг", "кккккккк"));
+    app.getStudentHelper().ModifyParentForm(new ParentData("Вова", "Ласточкин", "Венгрия", "Высоково", "(GMT-10:00) Тихий океан/Гонолулу", "Английский", "84958887856", "домик", null, "ггггггг", "89995632129", "74965477823", "telega", "бук", "контакт", "одноклассники", "стаг", "444ггггг", "кккккккк"));
     app.getStudentHelper().submitParentModify();
   }
 }
