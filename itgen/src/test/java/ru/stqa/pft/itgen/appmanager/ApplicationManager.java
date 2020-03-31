96805b1b9f3b5f78142a88e9c1aa26b564f3c9ad
Sergey Poletaev <info.elekit@yandex.ru>
2020-03-31 09:32:38 +0300

    реализована возможность удаленного запуска браузера

--- itgen/src/test/java/ru/stqa/pft/itgen/appmanager/ApplicationManager.java
+++ itgen/src/test/java/ru/stqa/pft/itgen/appmanager/ApplicationManager.java
@@ -1,17 +1,21 @@
 package ru.stqa.pft.itgen.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -26,8 +30,8 @@ public class ApplicationManager {
   private NavigationHelper navigationHelper;
   private String browser;
 
-  public ApplicationManager (String browser) {
-    this.browser=browser;
+  public ApplicationManager(String browser) {
+    this.browser = browser;
     properties = new Properties();
   }
 
@@ -35,25 +39,28 @@ public class ApplicationManager {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
-   if (browser.equals(BrowserType.FIREFOX)){
-     wd = new FirefoxDriver();
-   }
-   else if (browser.equals(BrowserType.CHROME)){
-     wd = new ChromeDriver();
-   }
-   else if (browser.equals(BrowserType.IE)){
-     wd = new InternetExplorerDriver();
-   }
-
-   wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-   wd.get(properties.getProperty("web.baseUrl"));
-   workerHelper = new WorkerHelper(wd);
-   sessionHelper = new SessionHelper(wd);
-   navigationHelper = new NavigationHelper(wd);
-   studentHelper = new StudentHelper(wd);
-   trainerHelper = new TrainerHelper(wd);
-   sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
-   }
+    if ("".equals(properties.getProperty("selenium.server"))) {
+      if (browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.IE)) {
+        wd = new InternetExplorerDriver();
+      }
+    } else {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setBrowserName(browser);
+      wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
+    }
+    wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+    wd.get(properties.getProperty("web.baseUrl"));
+    workerHelper = new WorkerHelper(wd);
+    sessionHelper = new SessionHelper(wd);
+    navigationHelper = new NavigationHelper(wd);
+    studentHelper = new StudentHelper(wd);
+    trainerHelper = new TrainerHelper(wd);
+    sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
+  }
 
   public void stop() {
     wd.quit();
