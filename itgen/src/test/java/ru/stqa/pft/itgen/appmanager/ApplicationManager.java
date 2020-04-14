ccabd9a665c7c5f951351d0c31329f8453647f82
Julja83 <julja83@list.ru>
2020-04-14 14:42:14 +0300

    Update ApplicationManager.java

--- itgen/src/test/java/ru/stqa/pft/itgen/appmanager/ApplicationManager.java
+++ itgen/src/test/java/ru/stqa/pft/itgen/appmanager/ApplicationManager.java
@@ -54,7 +54,7 @@ public class ApplicationManager {
       capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "windows")));
       wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
-    wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     wd.get(properties.getProperty("web.baseUrl"));
     workerHelper = new WorkerHelper(wd);
     sessionHelper = new SessionHelper(wd);
