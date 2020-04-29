1f3e168b12e1ae882eb9c0e76db2786dbdfd411f
Julja83 <julja83@list.ru>
2020-04-29 23:11:42 +0300

    Update ApplicationManager.java

--- itgen/src/test/java/ru/stqa/pft/itgen/appmanager/ApplicationManager.java
+++ itgen/src/test/java/ru/stqa/pft/itgen/appmanager/ApplicationManager.java
@@ -72,15 +72,6 @@ public class ApplicationManager {
     wd.quit();
   }
 
-  public boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
   public WorkerHelper getWorkerHelper() {
     return workerHelper;
   }
@@ -96,6 +87,7 @@ public class ApplicationManager {
   public StudentHelper students() {
     return studentHelper;
   }
+
   public ParentHelper parents() {
     return parentHelper;
   }
@@ -112,7 +104,6 @@ public class ApplicationManager {
     return ((TakesScreenshot) wd).getScreenshotAs(OutputType.BYTES);
   }
 
-
   public Screenshot getScreenShert(String locator) {
     Screenshot actualScreenshot;
     // return actualScreenshot = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(100)).takeScreenshot(wd);
@@ -120,6 +111,4 @@ public class ApplicationManager {
     wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(locator)));
     return new AShot().addIgnoredElement(By.xpath("//tbody")).takeScreenshot(wd);
   }
-
-
 }
