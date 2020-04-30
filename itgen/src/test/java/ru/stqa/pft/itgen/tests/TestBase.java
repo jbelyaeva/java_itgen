7c83e6f8ec89cdfb47b47de15e8d702b6749e30d
Julja83 <julja83@list.ru>
2020-04-30 10:38:36 +0300

    Update TestBase.java

--- itgen/src/test/java/ru/stqa/pft/itgen/tests/TestBase.java
+++ itgen/src/test/java/ru/stqa/pft/itgen/tests/TestBase.java
@@ -46,7 +46,7 @@ public class TestBase {
   public void logTestStop(Method m) {
     logger.info("Stop test " + m.getName());
   }
-  
+
   public void verifyStudentsListInUI() {
     if (Boolean.getBoolean("verifyUI")) {
       app.goTo().students();
