bbf54e58edb2e2e75d89fdff6c20bedcb07c5600
Julja83 <julja83@list.ru>
2020-05-13 11:04:34 +0300

    Update WorkerTrainerDataGenerator.java

--- itgen/src/test/java/ru/stqa/pft/itgen/generators/WorkerTrainerDataGenerator.java
+++ itgen/src/test/java/ru/stqa/pft/itgen/generators/WorkerTrainerDataGenerator.java
@@ -5,6 +5,7 @@ import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import ru.stqa.pft.itgen.model.TrainerData;
 import ru.stqa.pft.itgen.model.WorkerData;
 import ru.stqa.pft.itgen.tests.TestBase;
 
@@ -40,33 +41,63 @@ public class WorkerTrainerDataGenerator extends TestBase {
 
   //генерация тестовых данных
   private void run() throws IOException {
-    List<WorkerData> workers = generateWorkers(count);
+    List<TrainerData> trainers = generateTrainers(count);
     if (format.equals("json")) {
-      saveAsJson(workers, new File(file));
+      saveAsJson(trainers, new File(file));
     } else {
       System.out.println("Unrecognized format " + format);
     }
   }
 
   //сохранение этих данных в файл
-  private void saveAsJson(List<WorkerData> workers, File file) throws IOException {
+  private void saveAsJson(List<TrainerData> trainers, File file) throws IOException {
     Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
-    String json = gson.toJson(workers);
+    String json = gson.toJson(trainers);
     try (Writer writer = new FileWriter(file);) {
       writer.write(json);
     }
   }
-
-  private List<WorkerData> generateWorkers(int count) {
-    List<WorkerData> workers = new ArrayList<WorkerData>();
+  /**
+   * Инструкция:
+   * генератор настроен на создание данных для тестов на модификацию.
+   * при генерации данных для тестов на создание необходимо установить значения по умолчанию для следующих полей:
+   * gender -- 2,
+   * maxSlots -- 1,
+   * country -- "BY",
+   * city -- "Минск",
+   * timeZone -- "Europe/Minsk",
+   * locate -- "ru"
+   * payBase -- 5,
+   * note -- null,
+   * info -- null
+   * */
+  private List<TrainerData> generateTrainers(int count) {
+    List<TrainerData> trainers = new ArrayList<TrainerData>();
     for (int i = 0; i < count; i++) {
-      workers.add(new WorkerData()
+      trainers.add(new TrainerData()
               .withFirstName(String.format("Витя-%s", i))
               .withLastName(String.format("Ухов-%s", i))
-              .withEmailUI(String.format("eee+" + Math.round(Math.random() * 1000) + "@gmail.com", i))
-              .withPhone(String.format("89032566987", i))
-              .withRoleUi(String.format("Тренер", i)));
+       //       .withRoleUi(String.format("trainer", i))  // при создании сотрудника раскоментить
+              .withStartWorkUi(String.format("01.01.1998", i))
+              .withBirthdayUi(String.format("25.03.1979", i))
+              .withGender(Integer.valueOf(String.format("2", i)))
+              .withMaxSlots(Integer.valueOf(String.format("2", i)))
+              .withCountry(String.format("AL", i))
+              .withCity(String.format("Сватково %s", i))
+              .withTimeZone(String.format("Pacific/Honolulu", i))
+              .withLocate(String.format("ru", i))
+              .withPhone(String.format("11111111111", i))
+              .withTelegram(String.format("tg %s", i))
+              .withViber(String.format("22222222222", i))
+              .withSkype(String.format("sk %s", i))
+              .withWhatsapp(String.format("33333333333", i))
+              .withFb(String.format("fb %s", i))
+              .withVk(String.format("vk %s", i))
+              .withOk(String.format("ok %s", i))
+              .withInst(String.format("inst %s", i))
+              .withNote(String.format("Привет %s", i))
+              .withInfo(String.format("До свидания %s", i)));
     }
-    return workers;
+    return trainers;
   }
 }
