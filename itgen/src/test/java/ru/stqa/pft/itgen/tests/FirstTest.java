5a3f212932812304e8e180dc410fe0cff2511f1d
julja83 <59063644+julja83@users.noreply.github.com>
2020-06-09 10:25:15 +0300

    Добавлен пилотный первый тест

---
+++ itgen/src/test/java/ru/stqa/pft/itgen/tests/FirstTest.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.itgen.tests;
+
+import org.testng.annotations.Test;
+public class FirstTest extends TestBase {
+
+//пилотный тест, т.к. после git pull первый тест открывает кривую страницу после залогирования
+  @Test()
+  public void testLeadCreation() {
+    app.goTo().menuLeads();
+   }
+
+ }
