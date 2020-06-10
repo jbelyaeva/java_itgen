8dcf3961de0ef9b9ec90bdc469dec1a575ed9ebe
julja83 <59063644+julja83@users.noreply.github.com>
2020-06-10 16:17:38 +0300

    Изменено нажатие на иконку Тренеры в меню

--- itgen/src/test/java/ru/stqa/pft/itgen/appmanager/NavigationHelper.java
+++ itgen/src/test/java/ru/stqa/pft/itgen/appmanager/NavigationHelper.java
@@ -2,6 +2,10 @@ package ru.stqa.pft.itgen.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class NavigationHelper extends HelperBase {
 
@@ -18,7 +22,12 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void menuTrainers() {
-    click(By.xpath("//a[contains(@href, '/trainers')]"));
+    WebElement dynamicElement = (new WebDriverWait(wd, 10))
+            .until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(@href, '/trainers')]")));
+    Actions actions = new Actions(wd);
+    actions.moveToElement(dynamicElement).build().perform();
+    dynamicElement.click();
+
   }
 
   public void menuTasks() {
