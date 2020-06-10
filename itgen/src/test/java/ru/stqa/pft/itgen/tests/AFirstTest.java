40dc750e8c5a0ed410ff23b957eabf1518239053
julja83 <59063644+julja83@users.noreply.github.com>
2020-06-10 09:21:27 +0300

    Правка FirstTest

--- itgen/src/test/java/ru/stqa/pft/itgen/tests/AFirstTest.java
+++ itgen/src/test/java/ru/stqa/pft/itgen/tests/AFirstTest.java
@@ -1,11 +1,11 @@
 package ru.stqa.pft.itgen.tests;
 
 import org.testng.annotations.Test;
-public class FirstTest extends TestBase {
+public class AFirstTest extends TestBase {
 
 //пилотный тест, т.к. после git pull первый тест открывает кривую страницу после залогирования
   @Test()
-  public void testLeadCreation() {
+  public void testAFirst() {
     app.goTo().menuLeads();
    }
 
