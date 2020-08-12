df28358cf4da4b344aa5fe96ab97211b5752e137
julja83 <59063644+julja83@users.noreply.github.com>
2020-08-12 12:10:09 +0300

    Пока нет ответа от сервера

--- itgen/src/test/java/io/itgen/appmanager/StudentHelper.java
+++ itgen/src/test/java/io/itgen/appmanager/StudentHelper.java
@@ -4,6 +4,7 @@ import io.itgen.model.StudentData;
 import io.itgen.model.Students;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -250,11 +251,49 @@ public class StudentHelper extends HelperBase {
     }
   }
 
-
   public void logout() {
     click(By.xpath("//div[@class='arrow']"));
     click(By.xpath("//ul[contains(@class,'list')]/li[4]"));
   }
 
+  public void btnPay() {
+    click(By.xpath("//a[contains(@href,'/pay/')]"));
+  }
 
+  public void btnPay4Lessons() {
+    WebElement dynamicElement =
+        (new WebDriverWait(wd, 10))
+            .until(
+                ExpectedConditions.elementToBeClickable(
+                    By.xpath("//div[@class='family-product-item'][1]//a")));
+    dynamicElement.click();
+  }
+
+  public void fillTestCard() {
+    type(By.id("request_credit_card_number_1"), "4200");
+    type(By.id("request_credit_card_number_2"), "0000");
+    type(By.id("request_credit_card_number_3"), "0000");
+    type(By.id("request_credit_card_number_4"), "0000");
+    type(By.id("request_credit_card_exp_month"), "01");
+    type(By.id("request_credit_card_exp_year"), "25");
+    type(By.xpath("//input[contains(@placeholder,'IVAN')]"), "IVAN IVANOV");
+    type(By.xpath("//input[contains(@placeholder,'CVC')]"), "123");
+    type(By.xpath("//input[contains(@type,'email')]"), "julja@list.ru");
+    click(By.xpath("//input[contains(@name,'commit')]"));
+  }
+
+  public void paymentAdmin(String id) {
+    selectStudentInListUIById(id);
+    btnFamily();
+    btnPay();
+    btnPay4Lessons();
+    fillTestCard();
+  }
+
+  public Boolean findPictureSuccessPay(){
+    wd.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+    if (isElementPresent(By.xpath("//img[contains(@alt,'Success')]"))) return true;
+
+    return false;
+  }
 }
