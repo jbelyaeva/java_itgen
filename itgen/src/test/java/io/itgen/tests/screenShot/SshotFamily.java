9a38183ba83b01276ce12bd9fc287f44374e02ac
julja83 <59063644+julja83@users.noreply.github.com>
2020-08-14 09:24:08 +0300

    Стабилизация

--- itgen/src/test/java/io/itgen/tests/screenShot/SshotFamily.java
+++ itgen/src/test/java/io/itgen/tests/screenShot/SshotFamily.java
@@ -27,7 +27,7 @@ public class SshotFamily extends TestBase {
     app.family().btnFamily();
     app.sshot().changeTopBar();
 
-    ImageDiff diff =
+    ImageDiff diffFirst =
         app.sshot()
             .getImageDiff(
                 ApplicationManager.properties.getProperty("expected"),
@@ -35,6 +35,18 @@ public class SshotFamily extends TestBase {
                 ApplicationManager.properties.getProperty("markedImages"),
                 name,
                 locatorIgnor);
-    Assert.assertEquals(diff.getDiffSize(), 0);
+    if (diffFirst.getDiffSize() > 0) {
+      ImageDiff diffSecond =
+          app.sshot()
+              .getImageDiff(
+                  ApplicationManager.properties.getProperty("expected"),
+                  ApplicationManager.properties.getProperty("actual"),
+                  ApplicationManager.properties.getProperty("markedImages"),
+                  name,
+                  locatorIgnor);
+      Assert.assertEquals(diffSecond.getDiffSize(), 0);
+    } else {
+      Assert.assertEquals(diffFirst.getDiffSize(), 0);
+    }
   }
 }
